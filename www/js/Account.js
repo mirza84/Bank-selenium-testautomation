4e281486c6a5eedf23cd433f2702c9c1c1c267e4
DANIA MIRZA <daniamirza@laptop-h8i029qg.learnet.se>
2019-04-23 14:06:24 +0200

    added thomas fix for dates to work on english macs

--- www/js/Account.js
+++ www/js/Account.js
@@ -22,14 +22,23 @@ class Account {
   }
 
   formatTime(){
-    return new Intl.DateTimeFormat(
+
+    let d = new Date();
+    return d.getFullYear() + '-' 
+      + ((d.getMonth() + 1) + '').padStart(2,'0') + '-' 
+      + (d.getDate() + '').padStart(2,'0') + ' ' 
+      + (d.getHours() + '').padStart(2,0) + ':' 
+      + (d.getMinutes() + '').padStart(2, 0)
+
+    // why does this not work? 
+    /*return new Intl.DateTimeFormat(
       'se-SV', {
         year: 'numeric',
         month: 'numeric',
         day: 'numeric',
         hour: 'numeric',
         minute: 'numeric'
-    }).format(new Date());
+    }).format(new Date());*/
   }
 
 }
