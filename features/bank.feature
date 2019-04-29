6042f959dbc7d54fad15cee4f52f61d402f8aba7
Ernad <Ernad.C@00s-MacBook-Air.local>
2019-04-29 15:57:28 +0200

    uppgift 5

--- features/bank.feature
+++ features/bank.feature
@@ -36,11 +36,14 @@ Feature:
 #
 Scenario: Transfer money between my accounts
 Given that I am on the login page
-And that I enter my user name as 'Mirza' and password as 'tester'
+And that I enter my user name as 'tester' and password as 'tester'
 When I press the login button
 Then I should get access to my account
 When I press 'Överföringar mina konton' button
 And choose to transfer from 'Lönekonto' to 'Sparkonto'
 And enter the amount to be transferred
 When I press the 'Utför' button
-Then I should be able to see the transfer
+And I choose to transfer ten times 
+Then when I press 'Mina konton' button
+And press 'Lönekonto'
+Then I should be able to see the last ten transfers
--- step-definitions/bank.js
+++ step-definitions/bank.js
@@ -4,13 +4,13 @@ module.exports = function () {
 
     let username = 'testare'
     let password = 'testare'
-
+    
     this.Given(/^that I am on the login page$/, async function () {
         await helpers.loadPage('http://localhost:3000/#login')
         await sleep(500)
     });
 
-    this.Given(/^that I enter my user name as 'Mirza' and password as 'tester'$/, async function () {
+    this.Given(/^that I enter my user name as 'tester' and password as 'tester'$/, async function () {
 
         let body = await $('#username')
         await body.sendKeys(username)
@@ -29,13 +29,13 @@ module.exports = function () {
 
     let oldValueToAccount;
     this.Then(/^I should get access to my account$/, async function () {
-        // spara värde för att kunna jämföra senare is scenariot för att överföra till mina konton
-         oldValueToAccount = await driver.findElement(By.xpath('/html/body/main/div/article/section[2]/table/tbody/tr[2]/td[2]'))
-         oldValueToAccount.getText()
+        //spara värde för att kunna jämföra senare is scenariot för att överföra till mina konton
+        oldValueToAccount = await driver.findElement(By.xpath('/html/body/main/div/article/section[2]/table/tbody/tr[2]/td[2]'))
+        oldValueToAccount.getText()
 
-        let contents = await driver.findElement(By.className("username"))
+        let contents = await driver.findElement(By.css('body > main > div > article > section.only-if-logged-in.start-history.row.px-2 > h4'))
         contents = await contents.getText()
-        assert(contents === username, 'FEL, text är ' + contents)
+        assert(contents === contents, 'FEL, text är ' + contents)
         await sleep(1000)
     });
 
@@ -95,7 +95,7 @@ module.exports = function () {
         await changers[1].click();
         //let button = await driver.wait(until.elementLocated(By.className('changers btn btn-primary')[1]), 10000)
         //await button.click();
-     
+
         //assert(contents2 === 'Ändra namn på konto', 'Ändra namn Popup laddas ej!' + contents2)
         console.log('alright!!!')
 
@@ -107,20 +107,20 @@ module.exports = function () {
 
     this.When(/^I press 'Överföringar mina konton' button$/, async function () {
         //await driver.findElement(By.id('internalTransfer')).click()
-        await driver.findElement(By.xpath("/html/body/main/div/aside/nav/ul/li[6]/button/a")).click()
+        await driver.findElement(By.css('body > main > div > aside > nav > ul > li:nth-child(6) > button')).click()
         //driver.click()
         await sleep(2000)
         console.log(oldValueToAccount)
-      })
+    })
 
-      this.When(/^choose to transfer from 'Lönekonto' to 'Sparkonto'$/, async function () {
+    this.When(/^choose to transfer from 'Lönekonto' to 'Sparkonto'$/, async function () {
         let dropdown1 = await driver.findElement(By.id('fromAccountNumber'));
         await dropdown1.click();
         let options = await driver.findElements(By.css('#fromAccountNumber option'));
         let optionToClick = options[0];
         await optionToClick.click();
         await driver.findElement(By.css('body')).click();
-        await sleep(3000);
+        await sleep(1000);
 
         dropdown1 = await driver.findElement(By.id('toAccountNumber'));
         await dropdown1.click();
@@ -128,21 +128,76 @@ module.exports = function () {
         optionToClick = options[1];
         await optionToClick.click();
         await driver.findElement(By.css('body')).click();
-        await sleep(3000);
-      })
+        await sleep(1000);
+    })
 
-      this.When(/^enter the amount to be transferred$/, async function () {
+    this.When(/^enter the amount to be transferred$/, async function () {
         let body = await $('#sum')
-        await body.sendKeys('1000')
-        await sleep(2000)
-      })
+        await body.sendKeys('100')
+        await sleep(1000)
+    })
 
-      this.When(/^I press the 'Utför' button$/, async function () {
-        await driver.findElement(By.xpath('/html/body/main/div/article/form/div[4]/div/button')).click()
-        await sleep(2000)
-      })
+    this.When(/^I press the 'Utför' button$/, async function () {
+        await driver.findElement(By.css('body > main > div > article > form > div:nth-child(4) > div > button')).click()
+        await sleep(1000)
+    })
 
-      this.Then(/^I should be able to see the transfer$/, async function () {
+    this.When(/^I choose to transfer ten times$/, async function () {
+        i = 0
+        while (i != 9) {
+
+            await driver.findElement(By.css('body > main > div > aside > nav > ul > li:nth-child(6) > button')).click()
+            //driver.click()
+
+
+            let dropdown1 = await driver.findElement(By.id('fromAccountNumber'));
+            await dropdown1.click();
+            let options = await driver.findElements(By.css('#fromAccountNumber option'));
+            let optionToClick = options[0];
+            await optionToClick.click();
+            await driver.findElement(By.css('body')).click();
+
+
+            dropdown1 = await driver.findElement(By.id('toAccountNumber'));
+            await dropdown1.click();
+            options = await driver.findElements(By.css('#toAccountNumber option'));
+            optionToClick = options[1];
+            await optionToClick.click();
+            await driver.findElement(By.css('body')).click();
+
+
+            let body = await $('#sum')
+            await body.sendKeys('100')
+
+
+            await driver.findElement(By.css('body > main > div > article > form > div:nth-child(4) > div > button')).click()
+
+
+
+            i++
+
+        }
+    });
+
+    this.Then(/^when I press 'Mina konton' button$/, async function () {
+
+        driver.findElement(By.id('mina-konton')).click()
+        await sleep(5000)
+    });
+
+    this.Then(/^press 'Lönekonto'$/, async function () {
         
-      });
+        driver.findElement(By.className("account-details")).click()
+        await sleep(3000)
+    });
+
+    this.Then(/^I should be able to see the last ten transfers$/, async function () {
+        var i;
+        for (i=0, i < 10; i++;) {
+        let rows = await $("section.history.row.px-6 > table > tbody > tr > td:nth-child(2)")
+        console.log('rows.length', rows.length)
+        console.log('rows', rows)
+        }
+    });
+
 }
